fef2766d5d6fd0c284ba3fda14525e826a67abdc
MrVelasquezz <smyrnijoswest51@gmail.com>
2021-11-24 22:49:39 +0100

    .

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 
 const app = express()
+app.use(express.static('./dist'))
 
+/*app.get('/', (req, res) => {
+    console.log(1)
+    console.log(req)
+    res.sendFile(__dirname+'/dist/index.html')
+})*/
 
 app.listen(3000, () => console.log('Started'))
